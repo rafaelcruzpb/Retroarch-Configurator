8032a2791c164fb78550a9cb4177936c627ca563
Rafael Cruz <rafaelcruz@outlook.com>
2017-04-13 23:47:29 -0300

    bugfix

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ import os.path
 shutil.copy2('joystickconfig.py', '/home/pi/joystickconfig.py')
 
 if os.path.exists('retroarch.cfg'):
+	print "arquivo retroarch encontrado..."
 	shutil.copy2('retroarch.cfg', '/opt/retropie/configs/neogeo/retroarch.cfg')
 
 if os.path.exists('Retroarch Config Input.sh'):
+	print "arquivo retroarch config input encontrado..."
+	print "Configurando e instalando..."
 	os.system('chmod +x Retroarch\ Config\ Input.sh')
 	shutil.copy2('Retroarch Config Input.sh', '/home/pi/Desktop/RetroPie/retropiemenu/')
